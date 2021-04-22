b13cf5705549a0a9e936b79683f72848227f7512
junhee916 <junhee916@naver.com>
2021-04-22 21:42:44 +0900

    bcrypt setting

--- router/user.js
+++ router/user.js
@@ -1,29 +1,38 @@
 const express = require('express')
 const userModel = require('../model/user')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 // 회원가입
 router.post("/", (req, res) => {
 
-    const newUser = new userModel({
-        name : req.body.userName,
-        email : req.body.userEmail,
-        password : req.body.psd
-    })
-
-    newUser
-        .save()
-        .then(user => {
-            res.json({
-                msg : "registered user",
-                userInfo : user
-            })
-        })
-        .catch(err => {
-            res.status(500).json({
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err){
+            return res.status(404).json({
                 msg : err.message
             })
+        }
+        const newUser = new userModel({
+            name : req.body.userName,
+            email : req.body.userEmail,
+            password : hash
         })
+
+        newUser
+            .save()
+            .then(user => {
+                res.json({
+                    msg : "registered user",
+                    userInfo : user
+                })
+            })
+            .catch(err => {
+                res.status(500).json({
+                    msg : err.message
+                })
+            })
+    })
+
 })
 
 // 로그인
