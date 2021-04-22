b7d91c22d07730a3bcb90c672d98ae4df43b0670
junhee916 <junhee916@naver.com>
2021-04-22 21:57:33 +0900

    double checked email

--- router/user.js
+++ router/user.js
@@ -4,36 +4,56 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 
 // 회원가입
-router.post("/", (req, res) => {
+router.post("/signup", (req, res) => {
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err){
-            return res.status(404).json({
+    userModel
+        .findOne({email : req.body.userEmail})
+        .then(user => {
+            if(user){
+                return res.json({
+                    msg : "user existed, please other email"
+                })
+            }
+            else{
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err){
+                        return res.status(404).json({
+                            msg : err.message
+                        })
+                    }
+                    const newUser = new userModel({
+                        name : req.body.userName,
+                        email : req.body.userEmail,
+                        password : hash
+                    })
+
+                    newUser
+                        .save()
+                        .then(user => {
+                            res.json({
+                                msg : "registered user",
+                                userInfo : user
+                            })
+                        })
+                        .catch(err => {
+                            res.status(500).json({
+                                msg : err.message
+                            })
+                        })
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
                 msg : err.message
             })
-        }
-        const newUser = new userModel({
-            name : req.body.userName,
-            email : req.body.userEmail,
-            password : hash
         })
 
-        newUser
-            .save()
-            .then(user => {
-                res.json({
-                    msg : "registered user",
-                    userInfo : user
-                })
-            })
-            .catch(err => {
-                res.status(500).json({
-                    msg : err.message
-                })
-            })
-    })
 
 })
 
 // 로그인
+router.post("/login", (req, res) => {
+
+})
 module.exports = router
